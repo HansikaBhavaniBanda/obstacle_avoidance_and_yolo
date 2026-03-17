8c6d809c02857f0027272a4908a7e42bf9d3491f
Bhavana <dbhavana.arc@ndmatrix.in>
2026-03-17 17:10:23 +0530

    updated smoothing factor and Least Squares

--- backend/src/main/java/com/evtol/trajectoryengine/dto/TrajectoryResponse.java
+++ backend/src/main/java/com/evtol/trajectoryengine/dto/TrajectoryResponse.java
@@ -1,18 +1,24 @@
 package com.evtol.trajectoryengine.dto;
 
 import com.evtol.trajectoryengine.domain.TrajectoryPoint;
+import com.evtol.trajectoryengine.domain.Waypoint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @Data
+@CrossOrigin(origins = "*")
+@RestController
 @AllArgsConstructor
 @NoArgsConstructor
 public class TrajectoryResponse {
 
     private List<TrajectoryPoint> trajectory;
+    private List <Waypoint> waypoints;
     private double totalDuration;
 }
